ebf5ebb0d73d4d463d04aa711a2537c5db5464de
michael-j-girard <43144844+michael-j-girard@users.noreply.github.com>
2018-09-10 11:33:14 -0500

    Update Program.cs

--- Solution/HelloConsole/Program.cs
+++ Solution/HelloConsole/Program.cs
@@ -6,7 +6,7 @@ namespace HelloConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello NFCU");
         }
     }
 }
